1408991b45227dc41a5ab95773bbf6de3407646e
t2wole <t2wole@gmail.com>
2022-04-20 00:25:28 +0900

    first

---
+++ src/Ch01/hotkey.java
@@ -0,0 +1,22 @@
+package Ch01;
+
+public class hotkey {
+
+	public static void main(String[] args) {
+		// ctrl + d -> 줄 삭제
+		// ctrl + z -> 이전 모습으로
+		// ctrl + alt + up, down -> 행단위 복사
+		// alt + shift + a -> 멀티컬럼(shift 누르면서 방향키)
+		// alt + up, down -> 행단위 이동
+		// ctrl + i -> 자동 들여쓰기
+		// ctrl + space -> 자동완성
+		
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+
+	}
+
+}
